e7e05b7c6be14da990bc2f56855b8eb3fdc6ae97
mata704 <mata704@hotmail.com>
2022-11-25 22:47:39 +0000

    changes

---
+++ React/fundamentals/modulo1.js
@@ -0,0 +1,8 @@
+let newDiv = document.createElement('div');
+//Create div Atributes
+newDiv.append("Hello Edit")
+let root = document.getElementById("root")
+root.appendChild(newDiv);
+document.body.appendChild(newDiv)
+
+
